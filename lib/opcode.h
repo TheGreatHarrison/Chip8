7cc0c9ce80e54cd0545a233deacc586794d2496c
Harrison <harrison@dyetribe.co.nz>
2023-08-01 01:14:41 -0700

    Added opcode in memory with access to each op

---
+++ lib/opcode.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+union opcode
+{
+    u_int16_t instruction;
+
+    struct
+    {
+        
+        uint8_t n : 4; // A 4-bit number.
+        uint8_t y : 4; // Used to look up one of the 16 registers (VY) from V0 through VF
+        uint8_t x : 4; // Used to look up one of the 16 registers (VX) from V0 through VF.
+        uint8_t op : 4;
+    };
+
+    struct
+    {
+        uint8_t nn : 8; //  An 8-bit immediate number.
+    };
+
+    struct
+    {
+        uint16_t address : 12; // A 12-bit immediate memory address.
+    };
+
+};
